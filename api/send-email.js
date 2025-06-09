e8d74e22be59a54572c78dc5a6c32f7f9c352d8a
signordemola <adedamola4678@gmail.com>
2025-06-09 05:36:44 -0500

    update enc

--- api/send-email.js
+++ api/send-email.js
@@ -47,7 +47,7 @@ module.exports = async (req, res) => {
     to: "sales@qulaaengineering.com",
     subject: "New Contact Form Submission",
     html: `
-              <h3>New Message from Contact Form</h3>
+              <h3>New Message</h3>
               <p><strong>Name:</strong> ${name}</p>
               <p><strong>Company:</strong> ${company}</p>
               <p><strong>Telephone:</strong> ${tel}</p>
